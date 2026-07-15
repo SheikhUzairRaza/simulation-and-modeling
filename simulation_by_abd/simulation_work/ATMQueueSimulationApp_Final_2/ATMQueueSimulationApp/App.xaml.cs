7c7020a631ff8b5617baf12a6984bcfba5b830f1
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-16 00:29:52 +0500

    Create App.xaml.cs

---
+++ simulation_by_abd/simulation_work/ATMQueueSimulationApp_Final_2/ATMQueueSimulationApp/App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace ATMQueueSimulationApp
+{
+    /// <summary>
+    /// Application entry point. Nothing special here yet -
+    /// startup window is defined in App.xaml (StartupUri).
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
